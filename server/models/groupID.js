42741214c7ba01d60e611cac675716fc54f0f60d
gardenrose <44342235+gardenrose@users.noreply.github.com>
2024-09-27 12:00:56 +0200

    Delete server directory

--- server/models/groupID.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const groupIdSchema = new mongoose.Schema({
-  MAX_GROUP_ID: { type: Number, required: true, unique: true },
-});
-
-const GroupID = mongoose.model("GroupID", groupIdSchema, "groupID");
-
-module.exports = GroupID;

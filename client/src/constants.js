48f66b858cd4acfe17ea3daa227039e431863d3a
gardenrose <44342235+gardenrose@users.noreply.github.com>
2024-09-27 12:00:48 +0200

    Delete client directory

--- client/src/constants.js
+++
@@ -1,5 +0,0 @@
-export const INVALID_INPUT_WARNING = 'Enter a valid word with atleast one valid synonym.';
-export const CONFLICTING_INPUT_WARNING = 'A word with this context already exists.';
-export const INSERT_SUCCESSFUL = 'The word with its synonyms was successfully inserted.';
-export const REPEATING_SYNONYM_WARNING = 'Enter atleast one valid synonym, unique and not empty.';
-export const NO_SYNONYMS = 'No synonyms available)'; 

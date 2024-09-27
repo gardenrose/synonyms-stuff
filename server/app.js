405090f5164b1b8497aa583291c9e1adf2077246
gardenrose <44342235+gardenrose@users.noreply.github.com>
2024-09-27 11:11:42 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -35,7 +35,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 //app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("/", async (req, res, next) => {
